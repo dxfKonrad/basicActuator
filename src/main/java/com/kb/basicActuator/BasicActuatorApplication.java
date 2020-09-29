cefec37021e9a735d069420b4e9891a73eccfd5c
Konrad Banys <konrad.banys@digitalfrontiers.de>
2020-09-29 12:41:45 +0200

    implemented basic controller and added dockerfile

---
+++ src/main/java/com/kb/basicActuator/BasicActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.kb.basicActuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicActuatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kb/basicActuator/BasicActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kb.basicActuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
